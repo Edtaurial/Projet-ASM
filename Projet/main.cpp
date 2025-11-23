148c4f12aa07e43a8221917eea424e8b16d6aae1
Princial Nzeguim <princialnzeguim15@gmail.com>
2025-11-23 17:45:25 -0500

    taille fichiers

--- Projet/main.cpp
+++ Projet/main.cpp
@@ -17,7 +17,6 @@ extern "C" int GetProcessInfoASM(int index, int* id, int* taille, int* etat, voi
 extern "C" void* LancerProcessusASM(int id);
 extern "C" int FermerProcessusASM(int id);
 extern "C" void* UtiliserProcessusASM(int id);
-// <--- AJOUT ETAPE 7
 extern "C" int VerifierAccesASM(int id, void* addr);
 
 extern "C" long long GetRamUsageASM();
@@ -26,6 +25,8 @@ extern "C" long long GetMvUsageASM();
 extern "C" long long GetMvTotalASM();
 extern "C" void* GetPtrRAM();
 extern "C" void* GetPtrMV();
+extern "C" int GetActiveProgIDASM();
+extern "C" void SetActiveProgIDASM(int id);
 
 std::map<int, std::vector<std::string>> g_CodeSource;
 
@@ -72,19 +73,21 @@ void DessinerBarreAvecPlage(std::string titre, void* debut, long long total, int
     if (total > 0) pct = (double)utilise / total * 100.0;
 
     std::cout << " " << std::left << std::setw(20) << titre << " [Plage: " << debut << " - " << fin << "]\n";
-    std::cout << "   Usage: " << std::right << std::setw(6) << utilise << " / " << total << " octets (" << std::fixed << std::setprecision(1) << pct << "%)\n";
-    std::cout << "   [";
+    std::cout << " Etat: " << std::right << std::setw(6) << utilise << " / " << total << " octets \n";
+    /*std::cout << "   [";
     int width = 50;
     int pos = width * pct / 100;
     for (int i = 0; i < width; ++i) { if (i < pos) std::cout << "#"; else std::cout << "."; }
-    std::cout << "]\n\n";
+    std::cout << "]\n\n";*/
+    std::cout << "\n\n";
 }
 
 void AfficherTableauProcessus() {
+    int actifID = GetActiveProgIDASM();
     std::cout << " LISTE DES PROCESSUS\n";
-    std::cout << " +----+----------+---------------+------------------+------------------+\n";
-    std::cout << " | ID | TAILLE   | ETAT          | DEBUT (ADDR)     | FIN (ADDR)       |\n";
-    std::cout << " +----+----------+---------------+------------------+------------------+\n";
+    std::cout << " +----+----------+---------------------+------------------+------------------+\n";
+    std::cout << " | ID | TAILLE   | ETAT                | ADRESSE DE DEBUT | ADRESSE DE FIN   |\n";
+    std::cout << " +----+----------+---------------------+------------------+------------------+\n";
 
     int nb = GetNbProgsASM();
     for (int i = 0; i < nb; i++) {
@@ -94,60 +97,75 @@ void AfficherTableauProcessus() {
 
         std::string sEtat = "FERME";
         void* addrFin = nullptr;
-        if (etat == 1) sEtat = "EN RAM";
+
+        if (etat == 1) {
+            if (id == actifID) sEtat = ">> EN EXECUTION <<";
+            else sEtat = "OUVERT (EN RAM)";
+        }
         if (etat == 2) sEtat = "SWAPPE (MV)";
+
         if (etat != 0 && addrDebut != nullptr) addrFin = reinterpret_cast<char*>(addrDebut) + taille;
         else { addrDebut = 0; addrFin = 0; }
 
-        std::cout << " | " << std::left << std::setw(2) << id << " | " << std::setw(8) << taille << " | " << std::setw(13) << sEtat << " | " << std::setw(16) << addrDebut << " | " << std::setw(16) << addrFin << " |\n";
+        std::cout << " | " << std::left << std::setw(2) << id
+            << " | " << std::setw(8) << taille/1024
+            << " | " << std::setw(19) << sEtat
+            << " | " << std::setw(16) << addrDebut
+            << " | " << std::setw(16) << addrFin << " |\n";
     }
-    std::cout << " +----+----------+---------------+------------------+------------------+\n";
+    std::cout << " +----+----------+---------------------+------------------+------------------+\n";
 }
 
-void AfficherContenuRAM() {
-    std::cout << "\n CONTENU DE LA RAM (Instructions chargees)\n";
-    std::cout << " =========================================\n";
-    int nb = GetNbProgsASM();
-    bool unProgrammeEnRam = false;
-    for (int i = 0; i < nb; i++) {
-        int id, taille, etat;
-        void* addr;
-        GetProcessInfoASM(i, &id, &taille, &etat, &addr);
-        if (etat == 1) {
-            unProgrammeEnRam = true;
-            std::cout << " > PROGRAMME ID " << id << " (" << taille << " o) :\n   ---------------------\n";
-            if (g_CodeSource.count(id)) { for (const auto& ligne : g_CodeSource[id]) std::cout << "      " << ligne << "\n"; }
-            std::cout << "\n";
+void AfficherProgrammeActif() {
+    int actifID = GetActiveProgIDASM();
+    std::cout << "\n CPU (PROGRAMME EN COURS D'EXECUTION)\n";
+    std::cout << " ====================================\n";
+    if (actifID > 0) {
+        if (g_CodeSource.count(actifID)) {
+            std::cout << " > PROGRAMME ID " << actifID << " execute actuellement :\n";
+            std::cout << "   --------------------------------------\n";
+            std::cout << "   IP -> ";
+            for (const auto& ligne : g_CodeSource[actifID]) std::cout << ligne << "\n         ";
         }
     }
-    if (!unProgrammeEnRam) std::cout << " (Aucun programme en cours d'execution)\n";
-    std::cout << " =========================================\n";
+    else {
+        std::cout << " (IDLE - Aucun programme actif)\n";
+    }
+    std::cout << " ====================================\n";
 }
 
 int main() {
     try { InitNoyauASM(); }
     catch (...) { return -1; }
     InitialiserProgrammes();
-    std::string messageNotification = "Systeme initialise. Pret.";
+    std::string messageNotification = "Systeme initialise.";
 
     while (true) {
         system("cls");
+
+        // --- EN-TÊTE ---
         std::cout << "======================================================================\n";
         std::cout << "              SIMULATEUR DE MEMOIRE VIRTUELLE (OS)                    \n";
-        std::cout << "======================================================================\n";
-        std::cout << " >> MESSAGE : " << messageNotification << "\n";
         std::cout << "======================================================================\n\n";
 
+        // --- AFFICHAGE ETAT ---
         DessinerBarreAvecPlage("RAM PHYSIQUE", GetPtrRAM(), GetRamTotalASM(), 1);
-        DessinerBarreAvecPlage("FICHIER D'ECHANGE", GetPtrMV(), GetMvTotalASM(), 2);
-        AfficherTableauProcessus();
-        AfficherContenuRAM();
+        DessinerBarreAvecPlage("MEMOIRE VIRTUELLE", GetPtrMV(), GetMvTotalASM(), 2);
 
-        std::cout << "\n ACTIONS :\n";
-        std::cout << "  1. Charger (Nouveau Lancement)\n";
+        AfficherTableauProcessus();
+        AfficherProgrammeActif();
+
+        // --- ZONE DE NOTIFICATION (Déplacée en bas) ---
+        std::cout << "\n";
+        std::cout << " >>> DERNIER MESSAGE : " << messageNotification << "\n";
+        std::cout << " ----------------------------------------------------------------------\n";
+
+        // --- MENU ---
+        std::cout << " ACTIONS :\n";
+        std::cout << "  1. Charger (Lancer)\n";
         std::cout << "  2. Fermer (Liberer)\n";
-        std::cout << "  5. Utiliser / Activer (Swap-In si necessaire)\n";
-        std::cout << "  6. Demander un Acces Memoire (Test Protection)\n"; // <--- AJOUT ETAPE 7
+        std::cout << "  5. Utiliser / Activer (Mettre en Execution)\n";
+        std::cout << "  6. Test Acces Memoire\n";
         std::cout << "  9. Quitter\n";
         std::cout << "\n Choix > ";
 
@@ -159,10 +177,13 @@ int main() {
 
         if (choix == 9) break;
 
+        // Reset message par défaut si pas d'action
+        messageNotification = "En attente...";
+
         if (choix == 1) {
             std::cout << "ID a charger > "; int id; std::cin >> id;
             void* res = LancerProcessusASM(id);
-            if (res) messageNotification = "[SUCCES] Prog " + std::to_string(id) + " charge.";
+            if (res) messageNotification = "[SUCCES] Prog " + std::to_string(id) + " est maintenant EN EXECUTION.";
             else messageNotification = "[ERREUR] Echec chargement ID " + std::to_string(id);
         }
         else if (choix == 2) {
@@ -171,36 +192,42 @@ int main() {
             else messageNotification = "[ERREUR] ID introuvable.";
         }
         else if (choix == 5) {
-            std::cout << "ID a utiliser > "; int id; std::cin >> id;
+            std::cout << "ID a activer > "; int id; std::cin >> id;
             void* res = UtiliserProcessusASM(id);
-            if (res) messageNotification = "[ACTIF] Prog " + std::to_string(id) + " est maintenant en RAM.";
+            if (res) messageNotification = "[ACTIF] Prog " + std::to_string(id) + " a pris le controle (EXECUTION).";
             else messageNotification = "[ERREUR] Impossible d'activer ID " + std::to_string(id);
         }
-        else if (choix == 6) { // <--- LOGIQUE ETAPE 7
-            std::cout << "ID du programme qui demande l'acces > ";
-            int id; std::cin >> id;
+        /*else if (choix == 6) {
+            std::cout << "ID Prog > "; int id; std::cin >> id;
+            std::cout << "Adresse Hex > "; size_t addrVal; std::cin >> std::hex >> addrVal; std::cin >> std::dec;
+            int res = VerifierAccesASM(id, (void*)addrVal);
+            if (res == 1) messageNotification = "[ACCES OK] Adresse valide pour ce programme.";
+            else messageNotification = "[ACCES REFUSE] Violation d'acces (SegFault).";
+        }*/
+        else if (choix == 6) {
+            std::cout << "ID Prog > "; int id; std::cin >> id;
 
-            std::cout << "Adresse cible (Hex, ex: 000001D...) > ";
+            std::cout << "Adresse Hex (ex: 14000) > ";
             size_t addrVal;
-            std::cin >> std::hex >> addrVal; // Lecture hexadécimale
-            std::cin >> std::dec; // Remettre en décimal pour la suite
+            std::cin >> std::hex >> addrVal;
+            std::cin >> std::dec;
 
-            void* ptrCible = (void*)addrVal;
+            int res = VerifierAccesASM(id, (void*)addrVal);
 
-            int res = VerifierAccesASM(id, ptrCible);
-
+            // --- CORRECTION DE L'AFFICHAGE DES ERREURS ---
             if (res == 1) {
-                messageNotification = "[ACCES AUTORISE] L'adresse appartient bien au programme " + std::to_string(id);
+                messageNotification = "[ACCES OK] Adresse valide pour ce programme.";
             }
             else if (res == 0) {
-                messageNotification = "[ACCES REFUSE] SEGMENTATION FAULT ! Adresse hors limites.";
+                messageNotification = "[ACCES REFUSE] SEGMENTATION FAULT (Adresse hors limites).";
             }
             else if (res == -1) {
-                messageNotification = "[ERREUR] Le programme n'est pas charge en RAM (Page Fault).";
+                messageNotification = "[ACCES REFUSE] PAGE FAULT (Le programme n'est pas en RAM).";
             }
             else {
                 messageNotification = "[ERREUR] ID Programme inconnu.";
             }
+            // ---------------------------------------------
         }
     }
     LibererMemoireASM();
--- Projet/memory.asm
+++ Projet/memory.asm
@@ -1,30 +1,28 @@
 ; =========================================================
-; Module ASM - Noyau Final
-; Intègre : Alloc, Swap, Fermeture (Step 5) et VirtualFree
+; Module ASM - Noyau Final (Swapping Intelligent + Compactage)
 ; =========================================================
 
-; --- Importation des APIs Windows ---
 extern VirtualAlloc : proc
-extern VirtualFree  : proc  ; <--- OBLIGATOIRE POUR LIBERER
+extern VirtualFree  : proc
 
 .data
     align 8
     ptrRAM      QWORD 0         
     ptrMV       QWORD 0         
-    
     offsetRAM   QWORD 0         
     offsetMV    QWORD 0         
     
     MAX_PROGS   EQU 10
     TablePCB    BYTE 320 DUP(0) 
-    NbProgs     DWORD 0         
+    NbProgs     DWORD 0  
+    ActiveProgID DWORD 0       
     
-    ; --- PARAMETRES ---
-    TAILLE_RAM_BYTES EQU 2000      ; Petite taille pour tester le Swap
+    ; --- PARAMETRES DE TEST ---
+    TAILLE_RAM_BYTES EQU 4194304      
     TAILLE_MV_BYTES  EQU 10485760  
     
     MEM_COMMIT_RESERVE EQU 3000h
-    MEM_RELEASE        EQU 8000h   ; Code pour VirtualFree
+    MEM_RELEASE        EQU 8000h
     PAGE_READWRITE     EQU 4
 
 .code
@@ -54,14 +52,18 @@ GetPtrMV proc
     mov rax, [ptrMV]
     ret
 GetPtrMV endp
+GetActiveProgIDASM proc
+    mov eax, [ActiveProgID]
+    ret
+GetActiveProgIDASM endp
+SetActiveProgIDASM proc
+    mov [ActiveProgID], ecx
+    ret
+SetActiveProgIDASM endp
 
-; =========================================================
-; InitNoyauASM : Allocation (Step 1)
-; =========================================================
+; --- INIT & LIBERATION ---
 InitNoyauASM proc
     sub rsp, 28h
-
-    ; Alloc RAM
     mov rcx, 0
     mov rdx, TAILLE_RAM_BYTES
     mov r8, MEM_COMMIT_RESERVE
@@ -69,7 +71,6 @@ InitNoyauASM proc
     call VirtualAlloc
     mov [ptrRAM], rax
 
-    ; Alloc MV
     mov rcx, 0
     mov rdx, TAILLE_MV_BYTES
     mov r8, MEM_COMMIT_RESERVE
@@ -80,40 +81,31 @@ InitNoyauASM proc
     mov [offsetRAM], 0
     mov [offsetMV], 0
     mov [NbProgs], 0
+    mov [ActiveProgID], 0
 
     lea rdi, TablePCB
     mov rcx, 40
     xor rax, rax
     rep stosq
-
     add rsp, 28h
     ret
 InitNoyauASM endp
 
-; =========================================================
-; LibererMemoireASM : Le vrai VirtualFree (Step 5 / Fin)
-; C'est ici qu'on rend physiquement la mémoire à Windows
-; =========================================================
 LibererMemoireASM proc
     sub rsp, 28h
-    
-    ; 1. Libération RAM
     mov rcx, [ptrRAM]
     test rcx, rcx
     jz FreeMV
-    xor rdx, rdx            ; Taille = 0 (Car MEM_RELEASE libère tout le bloc)
-    mov r8, MEM_RELEASE     ; Flag MEM_RELEASE
-    call VirtualFree        ; <--- APPEL OFFICIEL
-
+    xor rdx, rdx
+    mov r8, MEM_RELEASE
+    call VirtualFree
 FreeMV:
-    ; 2. Libération MV
     mov rcx, [ptrMV]
     test rcx, rcx
     jz FinLib
     xor rdx, rdx
     mov r8, MEM_RELEASE
-    call VirtualFree        ; <--- APPEL OFFICIEL
-
+    call VirtualFree
 FinLib:
     add rsp, 28h
     ret
@@ -121,6 +113,7 @@ LibererMemoireASM endp
 
 ; --- GESTION PCB ---
 EnregistrerProcessusASM proc
+    shl edx, 10
     mov r8d, [NbProgs]
     cmp r8d, MAX_PROGS
     jge FinEnreg
@@ -165,20 +158,12 @@ GetNbProgsASM proc
     ret
 GetNbProgsASM endp
 
-; =========================================================
-; FermerProcessusASM (Step 5)
-; Marque l'espace comme libre (Mise à zéro)
-; Note: On ne peut pas utiliser VirtualFree ici sur un sous-bloc
-; sans détruire toute la RAM allouée en Step 1.
-; =========================================================
 FermerProcessusASM proc
     push rdi
     push rax
-
     xor r8, r8
     mov r9d, [NbProgs]
     lea r10, TablePCB
-
 BoucleRechFermer:
     cmp r8d, r9d
     jge IntrouvableFermer
@@ -187,9 +172,7 @@ BoucleRechFermer:
     add r10, 32
     inc r8
     jmp BoucleRechFermer
-
 TrouveFermer:
-    ; On efface les données (remplit de 0) pour simuler la libération
     mov rdi, [r10+16]
     mov ecx, [r10+4]
     test rdi, rdi
@@ -197,16 +180,18 @@ TrouveFermer:
     xor eax, eax
     cld
     rep stosb
-
 Marquer:
-    mov dword ptr [r10+8], 0 ; ETAT = FERME
-    mov qword ptr [r10+16], 0; ADRESSE = NULL
-    
+    mov dword ptr [r10+8], 0
+    mov qword ptr [r10+16], 0
+    mov eax, [r10]
+    cmp [ActiveProgID], eax
+    jne PasActif
+    mov [ActiveProgID], 0
+PasActif:
     mov rax, 1
     pop rax
     pop rdi
     ret
-
 IntrouvableFermer:
     xor rax, rax
     pop rax
@@ -214,51 +199,139 @@ IntrouvableFermer:
     ret
 FermerProcessusASM endp
 
-; --- SWAPPING ---
-SwapperToutVersMV proc
+; =========================================================
+; EjecterPremierProgramme (NOUVEAU - SWAP INTELLIGENT)
+; Rôle : Trouve le programme situé tout au début de la RAM,
+;        le déplace en MV, et DÉCALE (Compacte) le reste.
+; =========================================================
+EjecterPremierProgramme proc
     push rsi
     push rdi
     push rbx
+    push r12
+    push r13
+
+    ; 1. Trouver la "Victime" : C'est celui dont Adresse == ptrRAM
     xor r8, r8
     mov r9d, [NbProgs]
-    lea r10, TablePCB 
+    lea r10, TablePCB
+    mov r12, [ptrRAM]       ; Adresse cible (Début RAM)
+    xor r13, r13            ; Pointeur vers PCB victime (0 = pas trouvé)
 
-BoucleSwap:
+ChercherVictime:
     cmp r8d, r9d
-    jge FinSwap
-
-    cmp dword ptr [r10+8], 1
-    jne Prochain
-
-    mov rsi, [r10+16]
-    mov rdi, [ptrMV]
-    add rdi, [offsetMV]
-    mov ecx, [r10+4]
-    cld
-    rep movsb
-
-    mov rax, [ptrMV]
-    add rax, [offsetMV]
-    mov [r10+16], rax
-    mov dword ptr [r10+8], 2 
-
-    mov eax, [r10+4]
-    add [offsetMV], rax
-
-Prochain:
+    jge FinRechercheVictime
+    
+    cmp dword ptr [r10+8], 1 ; Est-il en RAM ?
+    jne SuivantVictime
+    
+    cmp [r10+16], r12       ; Est-il au tout début (offset 0) ?
+    je TrouveVictime
+    
+SuivantVictime:
     add r10, 32
     inc r8
-    jmp BoucleSwap
+    jmp ChercherVictime
 
-FinSwap:
-    mov [offsetRAM], 0
+TrouveVictime:
+    mov r13, r10            ; R13 contient le PCB du programme à éjecter
+    
+FinRechercheVictime:
+    test r13, r13
+    jz FinEjection          ; Si pas trouvé (RAM vide ou bug), on sort
+
+    ; --- 2. COPIER VICTIME VERS MV ---
+    mov rsi, [r13+16]       ; Source (RAM)
+    mov rdi, [ptrMV]        ; Dest (MV)
+    add rdi, [offsetMV]     ; Offset MV
+    mov ecx, [r13+4]        ; Taille
+    
+    ; Copie RAM -> MV
+    push rcx                ; Sauve taille pour plus tard
+    cld
+    rep movsb
+    pop rcx                 ; Récupère taille (dans RCX)
+
+    ; Mise à jour PCB Victime
+    mov rax, [ptrMV]
+    add rax, [offsetMV]
+    mov [r13+16], rax       ; Nouvelle adresse (MV)
+    mov dword ptr [r13+8], 2 ; ETAT = SWAPPE
+    
+    ; Si c'était l'actif, reset
+    mov eax, [r13]
+    cmp [ActiveProgID], eax
+    jne UpdateMV
+    mov [ActiveProgID], 0
+
+UpdateMV:
+    mov eax, [r13+4]
+    add [offsetMV], rax     ; Avance Offset MV
+
+    ; --- 3. COMPACTAGE DE LA RAM (SHIFT) ---
+    ; On doit décaler tout ce qui était APRES la victime vers le HAUT (vers 0)
+    ; Taille du trou = Taille Victime (dans [r13+4])
+    mov r12d, [r13+4]       ; R12 = Taille du trou (Shift Amount)
+    
+    ; Calculer combien d'octets il faut bouger
+    ; BytesToMove = offsetRAM - TailleVictime
+    mov rcx, [offsetRAM]
+    sub rcx, r12
+    
+    cmp rcx, 0
+    jle FinCompactage       ; Rien à bouger (la RAM est vide après ce prog)
+
+    ; Déplacement Mémoire (memmove)
+    ; Destination = ptrRAM
+    mov rdi, [ptrRAM]
+    ; Source = ptrRAM + TailleVictime
+    mov rsi, [ptrRAM]
+    add rsi, r12
+    
+    cld
+    rep movsb               ; Décale tout vers le bas
+
+    ; --- 4. MISE A JOUR DES AUTRES PCB ---
+    ; Tous les programmes qui sont EN RAM doivent voir leur adresse reculer de R12
+    xor r8, r8
+    mov r9d, [NbProgs]
+    lea r10, TablePCB
+
+BoucleUpdateAdresse:
+    cmp r8d, r9d
+    jge FinUpdateAdresses
+    
+    cmp dword ptr [r10+8], 1 ; Si EN RAM
+    jne NextUpdate
+    
+    ; Adresse = Adresse - TailleTrou
+    mov rax, [r10+16]
+    sub rax, r12
+    mov [r10+16], rax
+
+NextUpdate:
+    add r10, 32
+    inc r8
+    jmp BoucleUpdateAdresse
+
+FinUpdateAdresses:
+
+FinCompactage:
+    ; --- 5. REDUIRE OFFSET RAM ---
+    sub [offsetRAM], r12
+
+FinEjection:
+    pop r13
+    pop r12
     pop rbx
     pop rdi
     pop rsi
     ret
-SwapperToutVersMV endp
+EjecterPremierProgramme endp
 
-; --- LANCEMENT ---
+; =========================================================
+; LancerProcessusASM
+; =========================================================
 LancerProcessusASM proc
     xor r8, r8
     mov r9d, [NbProgs]
@@ -277,31 +350,34 @@ Trouve:
     cmp dword ptr [r10+8], 1
     je DejaOuvert
 
-    mov r11d, [r10+4]
+    mov r11d, [r10+4]           ; Taille demandée
     cmp r11d, TAILLE_RAM_BYTES
     ja TropGros
 
-VerifierPlace:
+VerifierPlaceLancer:
     mov rax, [offsetRAM]
     mov rdx, rax
     add rdx, r11
     cmp rdx, TAILLE_RAM_BYTES
-    ja PasDePlace
+    ja PasDePlaceLancer         ; -> EJECTION INTELLIGENTE
 
+    ; Place OK
     mov rax, [ptrRAM]
     add rax, [offsetRAM]
     mov [r10+16], rax
     mov dword ptr [r10+8], 1
     add [offsetRAM], r11
+    mov [ActiveProgID], ecx
     ret
 
-PasDePlace:
+PasDePlaceLancer:
+    ; Au lieu de tout vider, on éjecte juste le premier (FIFO)
     push r10
     push r11
-    call SwapperToutVersMV
+    call EjecterPremierProgramme
     pop r11
     pop r10
-    jmp VerifierPlace
+    jmp VerifierPlaceLancer     ; On réessaie (peut-être qu'il faut en éjecter un 2ème ?)
 
 Introuvable:
     xor rax, rax
@@ -314,106 +390,75 @@ TropGros:
     ret
 LancerProcessusASM endp
 
-
-; ---------------------------------------------------------
-; UtiliserProcessusASM (Etape 6 - Swap In)
-; Rôle : Rend un programme actif. S'il est en MV, le ramène en RAM.
-; Entrée : RCX = ID
-; ---------------------------------------------------------
+; =========================================================
+; UtiliserProcessusASM
+; =========================================================
 UtiliserProcessusASM proc
     push rsi
     push rdi
     push rbx
-
-    ; 1. Trouver le programme
     xor r8, r8
     mov r9d, [NbProgs]
     lea r10, TablePCB
-
 BoucleRechUtil:
     cmp r8d, r9d
     jge IntrouvableUtil
-
     cmp [r10], ecx
     je TrouveUtil
-    
     add r10, 32
     inc r8
     jmp BoucleRechUtil
 
 TrouveUtil:
-    ; Cas A : Déjà en RAM ?
     cmp dword ptr [r10+8], 1
     je EstDejaEnRam
-
-    ; Cas B : Est-il Fermé ?
     cmp dword ptr [r10+8], 0
     je EstFerme
 
-    ; Cas C : Il est SWAPPE (Etat 2). On doit le ramener.
-    ; "ramenez-le en RAM"
-    
-    ; 1. Vérifier si on a la place en RAM maintenant
-    mov r11d, [r10+4]       ; Taille
+    ; Swap In requis
+    mov r11d, [r10+4]       ; Taille prog à ramener
+
+VerifierPlaceUtil:
     mov rax, [offsetRAM]
     add rax, r11
     cmp rax, TAILLE_RAM_BYTES
-    ja PasDePlaceRAM        ; Si pas de place -> On vide la RAM d'abord
+    ja PasDePlaceUtil       ; -> EJECTION INTELLIGENTE
 
-    ; 2. Copier MV -> RAM
-ExecuterSwapIn:
-    ; Source : Adresse actuelle en MV
+    ; Copie MV -> RAM
     mov rsi, [r10+16]       
-    
-    ; Destination : Base RAM + Offset RAM
     mov rdi, [ptrRAM]
     add rdi, [offsetRAM]
-    
-    ; Taille
     mov ecx, [r10+4]
-    
     cld
     rep movsb
 
-    ; 3. Mettre à jour PCB
     mov rax, [ptrRAM]
     add rax, [offsetRAM]
-    mov [r10+16], rax       ; Nouvelle Adresse (RAM)
-    mov dword ptr [r10+8], 1; Nouvel Etat (EN RAM)
-
-    ; 4. Avancer Offset RAM
+    mov [r10+16], rax
+    mov dword ptr [r10+8], 1
     mov eax, [r10+4]
     add [offsetRAM], rax
-
-    ; Retourne la nouvelle adresse
     mov rax, [r10+16]
+    mov [ActiveProgID], ecx
     jmp FinUtil
 
-PasDePlaceRAM:
-    ; La RAM est pleine. On doit swapper les autres vers la MV pour faire de la place.
-    ; ATTENTION : On doit sauvegarder le pointeur R10 car le call peut utiliser des registres
+PasDePlaceUtil:
     push r10
     push r11
-    
-    call SwapperToutVersMV  ; Vide la RAM (offsetRAM revient à 0)
-    
+    call EjecterPremierProgramme ; Libère juste ce qu'il faut
     pop r11
     pop r10
-    
-    ; Maintenant que la RAM est vide, on procède à la copie (Swap In)
-    jmp ExecuterSwapIn
+    jmp VerifierPlaceUtil
 
 EstDejaEnRam:
-    mov rax, [r10+16]       ; Retourne adresse actuelle
+    mov rax, [r10+16]
+    mov [ActiveProgID], ecx
     jmp FinUtil
-
 EstFerme:
-    xor rax, rax            ; Erreur (Fermé)
+    xor rax, rax
     jmp FinUtil
-
 IntrouvableUtil:
     xor rax, rax
-
 FinUtil:
     pop rbx
     pop rdi
@@ -421,64 +466,39 @@ FinUtil:
     ret
 UtiliserProcessusASM endp
 
-
-; ---------------------------------------------------------
-; VerifierAccesASM (Etape 7 - Protection Memoire)
-; Rôle : Vérifie si une adresse appartient à un programme
-; Entrée : RCX = ID du programme
-;          RDX = Adresse Cible (Pointeur)
-; Retour : RAX = 1 (Autorisé), 0 (Refusé/SegFault), -1 (Non chargé)
-; ---------------------------------------------------------
 VerifierAccesASM proc
-    ; 1. Trouver le programme
     xor r8, r8
     mov r9d, [NbProgs]
     lea r10, TablePCB
-
 BoucleRechAcces:
     cmp r8d, r9d
     jge IntrouvableAcces
-
     cmp [r10], ecx
     je TrouveAcces
-    
     add r10, 32
     inc r8
     jmp BoucleRechAcces
-
 TrouveAcces:
-    ; 2. Vérifier si le programme est bien en RAM (Etat = 1)
     cmp dword ptr [r10+8], 1
     jne PasEnRam
-
-    ; 3. Vérification des bornes (PROTECTION) 
-    ; Borne Inférieure : Adresse Debut
-    mov rax, [r10+16]       ; Base Address
+    mov rax, [r10+16]
     cmp rdx, rax
-    jb AccesRefuse          ; Si Cible < Debut => Erreur
-
-    ; Borne Supérieure : Adresse Debut + Taille
-    mov r11d, [r10+4]       ; Taille
-    add rax, r11            ; RAX = Fin (Exclusive)
+    jb AccesRefuse
+    mov r11d, [r10+4]
+    add rax, r11
     cmp rdx, rax
-    jae AccesRefuse         ; Si Cible >= Fin => Erreur
-
-    ; --- SUCCES ---
-    mov rax, 1              ; Accès Autorisé
+    jae AccesRefuse
+    mov rax, 1
     ret
-
 AccesRefuse:
-    xor rax, rax            ; Return 0 (Segmentation Fault)
+    xor rax, rax
     ret
-
 PasEnRam:
-    mov rax, -1             ; Return -1 (Erreur: Programme non chargé ou swappé)
+    mov rax, -1
     ret
-
 IntrouvableAcces:
-    mov rax, -2             ; ID Inconnu
+    mov rax, -2
     ret
 VerifierAccesASM endp
-
 
 end
